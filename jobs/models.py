5cecdc8f7a20bf3bc0431f7110eb9f71f1136269
Chayan Datta <sonicxxx7@gmail.com>
2018-06-07 23:22:11 +0530

    First Migration in Jobs

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    title=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='images/')
+    summary=models.CharField(max_length=250)
+    pass
+def __str__(self):
+    return self.title
+    pass
