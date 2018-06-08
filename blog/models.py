f9ecba09dc963b9841ab2365953d491310829582
Chayan Datta <sonicxxx7@gmail.com>
2018-06-08 15:22:52 +0530

    Blog Model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class blog(models.Model):
+    title=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='images/')
+    publish=models.DateTimeField()
+    body=models.TextField(max_length='300')
+    pass
+
+def __str__(self):
+    return self.title
+    pass
