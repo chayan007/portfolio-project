ac71e4f3b6ffaa9a116f93aa56d7b8d4d3e33ee6
Chayan Datta <sonicxxx7@gmail.com>
2018-06-08 20:30:48 +0530

    Blogs and Individual Page created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import blog
 
-# Create your views here.
+def allblog(request):
+    blogs=blog.objects
+    return render(request, 'blog/allblogs.html',{'blogs':blogs})
+    pass
+
+def detail(request,blog_id):
+    details=get_object_or_404(blog, pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':details})
+    pass
